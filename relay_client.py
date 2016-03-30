49d11d3febe0cba8bac74a3379d23ec593072e9c
iot195b <thinh93@yahoo.com>
2016-03-30 15:17:33 -0700

    Update relay_client.py

--- relay_client.py
+++ relay_client.py
@@ -10,21 +10,19 @@ while(true):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     	server_address = ('54.191.150.101', 9801)
     	sock.connect(server_address)
-		try:
+	try:
 		while True:
-            data = connection.recv(16)
-            print data
-            break
-    finally:
-        try:
-                if data=='ON':
-                                GPIO.output(18,True)
+        		data = connection.recv(16)
+			print data
+        		break
+    	finally:
+        	try:
+                	if data=='ON':
+                		GPIO.output(18,True)
 				time.sleep(0.2)
 				GPIO.output(18,False)
-                elif data=='OFF':
-                                GPIO.output(23,True)
+                	elif data=='OFF':
+                		GPIO.output(23,True)
 				time.sleep(0.2)
 				GPIO.output(23,False)
-
-						
-		else: print 'error'
+				else: print 'error'
