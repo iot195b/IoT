aa62237c4066a398c2759b27553ed15a09c60782
iot195b <thinh93@yahoo.com>
2016-03-30 14:58:18 -0700

    Added files via upload

---
+++ relay_client.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO, time
+import urllib2
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+true = 1
+
+while(true):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    	server_address = ('54.191.150.101', 9801)
+    	sock.connect(server_address)
+		try:
+		while True:
+            data = connection.recv(16)
+            print data
+            break
+    finally:
+        try:
+                if data=='ON':
+                                GPIO.output(18,True)
+				time.sleep(0.2)
+				GPIO.output(18,False)
+                elif data=='OFF':
+                                GPIO.output(23,True)
+				time.sleep(0.2)
+				GPIO.output(23,False)
+
+						
+		else: print 'error'
---
+++ relay_server.py
@@ -0,0 +1,44 @@
+import socket
+import sys
+import mysql.connector 
+from mysql.connector import errorcode
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address given on the command line
+
+server_address = ('', 9801)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+sock.listen(1)
+
+while True:
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+	try:
+        cnx=mysql.connector.connect(user= 'root',password= 'sheridan1',host= 'localhost',database= 'iot')
+        except mysql.connector.Error as e:
+            if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print ("username/password error")
+            else:
+                print(e)
+        cur= cnx.cursor()
+        sql= "SELECT * from relay where id= 1"
+        cur.execute(sql)
+		results= cursor.fetchall()
+		for row in results:
+		data= row[1]	
+        cur.close()
+        cnx.close()
+        connection.close()
+    try:
+        print >>sys.stderr, 'client connected:', client_address
+        message = data
+    	print >>sys.stderr, 'sending "%s"' % message
+    	sock.sendall(message)
+        print >>sys.stderr, 'received "%s"' % data
+		data = sock.recv(16)
+		print data
+    finally:
+		sock.close()
